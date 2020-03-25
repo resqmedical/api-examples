0915c0d26ace54c6ba2af2af1fa1387addaf7147
Robert Wilkerson <robert@resqmedical.com>
2020-03-25 14:05:38 -0700

    Adds script to generate simple user stats

---
+++ user-stats.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+import datetime
+import json
+import requests
+
+from dateutil.relativedelta import relativedelta
+
+if __name__ == '__main__':
+
+    credentials = {
+        'api_key': 'FIXME',
+        'inst_id': 'FIXME'
+    }
+
+    request = {
+        'credentials': credentials
+    }
+
+    response = requests.post(
+        'https://api.resqmedical.com/users/',
+        json=json.dumps(request),
+        verify=False)
+
+    if response.status_code != 200:
+        print(response.status_code)
+        print(response.json()['message'])
+    else:
+        type_counts = dict()
+        type_counts['active users'] = 0
+        type_counts['residents'] = 0
+        type_counts['attendings'] = 0
+        type_counts['nurses'] = 0
+        type_counts['program admins'] = 0
+        type_counts['gme admins'] = 0
+
+        users = response.json()
+        for user in users:
+            if user['is_active'] is True:
+                type_counts['active users'] += 1
+            if user['type'] == 'Resident':
+                type_counts['residents'] += 1
+            if user['type'] == 'Attending':
+                type_counts['attendings'] += 1
+            if user['type'] == 'Nurse':
+                type_counts['nurses'] += 1
+            if user['type'] == 'Program Admin':
+                type_counts['program admins'] += 1
+            if user['type'] == 'Institute Admin':
+                type_counts['gme admins'] += 1
+
+        for key, value in type_counts.items():
+            print('{} {} ({}%)'.format(value, key, round(100*value/len(users))))
